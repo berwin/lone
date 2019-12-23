1ab779dfc4150048003fc342c436f8401e8ebeea
zhanping <raozhanping@gmail.com>
2019-12-23 19:57:05 +0800

    fix: fix handle error in one env

--- packages/lone-util/error.js
+++ packages/lone-util/error.js
@@ -1,17 +1,10 @@
-import { noop } from '.'
 import { inWorker } from './env'
 
-export let warn = noop
-export let tip = noop
-export let formatComponentName = noop
-export let generateComponentTrace = noop
-
 export function handleError (err, vm, info) {
-  // FIXME: errorCaptured
-  if (process.env.NODE_ENV !== 'production') {
-    warn(`Error in ${info}: "${err.toString()}"`, vm)
-  }
+  // show friendly error log
+  warn(`Error in ${info}: "${err.toString()}"`, vm)
   /* istanbul ignore else */
+  // show error stack log
   if (inWorker && typeof console !== 'undefined') {
     console.error(err)
   } else {
@@ -19,35 +12,33 @@ export function handleError (err, vm, info) {
   }
 }
 
-if (process.env.NODE_ENV !== 'production') {
-  const hasConsole = typeof console !== 'undefined'
-  const classifyRE = /(?:^|[-_])(\w)/g
-  const classify = str =>
-    str.replace(classifyRE, c => c.toUpperCase()).replace(/[-_]/g, '')
+const hasConsole = typeof console !== 'undefined'
+const classifyRE = /(?:^|[-_])(\w)/g
+const classify = str =>
+  str.replace(classifyRE, c => c.toUpperCase()).replace(/[-_]/g, '')
 
-  warn = (msg, vm) => {
-    const trace = vm ? generateComponentTrace(vm) : ''
+export function warn (msg, vm) {
+  const trace = vm ? generateComponentTrace(vm) : ''
 
-    if (hasConsole) {
-      console.error(`[Lone warn]: ${msg}${trace}`)
-    }
+  if (hasConsole) {
+    console.error(`[Lone warn]: ${msg}${trace}`)
   }
+}
 
-  tip = (msg, vm) => {
-    const trace = vm ? generateComponentTrace(vm) : ''
+export function tip (msg, vm) {
+  const trace = vm ? generateComponentTrace(vm) : ''
 
-    if (hasConsole) {
-      console.warn(`[Lone tip]: ${msg}${trace}`)
-    }
+  if (hasConsole) {
+    console.warn(`[Lone tip]: ${msg}${trace}`)
   }
+}
 
-  formatComponentName = vm => {
-    const name = vm.$options.name
+export function formatComponentName (vm) {
+  const name = vm.$options.name
 
-    return (name ? `<${classify(name)}>` : '<Anonymous>')
-  }
+  return (name ? `<${classify(name)}>` : '<Anonymous>')
+}
 
-  generateComponentTrace = vm => {
-    return `\n\n(found in ${formatComponentName(vm)})`
-  }
+export function generateComponentTrace (vm) {
+  return `\n\n(found in ${formatComponentName(vm)})`
 }
