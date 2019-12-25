c2e1efc0615b58f4bf2dca68ddc21f3fded5a6bf
zhanping <raozhanping@gmail.com>
2019-12-25 15:18:02 +0800

    style: fix error, fix #3

--- packages/lone-util/error.js
+++ packages/lone-util/error.js
@@ -2,7 +2,6 @@ export function handleError (err, vm, info) {
   warn(`Error in ${info}: "${err.toString()}"`, vm, err)
 }
 
-const hasConsole = typeof console !== 'undefined'
 const classifyRE = /(?:^|[-_])(\w)/g
 const classify = str =>
   str.replace(classifyRE, c => c.toUpperCase()).replace(/[-_]/g, '')
@@ -11,29 +10,20 @@ export function warn (msg, vm, err) {
   const trace = vm ? generateComponentTrace(vm) : ''
   const hasErr = err
 
-  if (!err) {
-    err = err || { toString () { return this.name + this.message } }
-  }
-
+  err = err || { toString () { return this.name + this.message } }
   err.name = '[Lone warn]'
   err.message = ` ${msg}${trace}`
 
-  if (hasConsole) {
-    console.error(hasErr ? err : String(err))
-  }
+  console.error(hasErr ? err : String(err))
 }
 
 export function tip (msg, vm) {
   const trace = vm ? generateComponentTrace(vm) : ''
-
-  if (hasConsole) {
-    console.warn(`[Lone tip]: ${msg}${trace}`)
-  }
+  console.warn(`[Lone tip]: ${msg}${trace}`)
 }
 
 export function formatComponentName (vm) {
   const name = vm.$options.name
-
   return (name ? `<${classify(name)}>` : '<Anonymous>')
 }
 
